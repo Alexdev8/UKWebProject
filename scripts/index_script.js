45d1481873144247e39692585bc4ed399f025e1f
Alexdev8 <alexisbensoussan06@gmail.com>
2023-04-07 23:29:59 +0100

    amelioration de la searchbox, elle reste maintenant ouverte si l'on tape dedans et disparait si on perd le focus si elle est vide

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -1,20 +1,19 @@
-// let search_box = document.getElementById("search-input");
-// let search_btn = document.getElementById("search-btn");
-// let search_div = document.getElementById("search-div");
-//
-// let search_box_opened = false;
-// function openSearchBox() {
-//     if (!search_box_opened) {
-//         search_box_opened = true;
-//         // search_box.style.display = "inline-block";
-//         search_box.classList.toggle("show");
-//         console.log(search_box.classList);
-//         search_div.style.padding = "0.3em";
-//         search_div.style.boxShadow = "2px 2px 6px rgb(26,26,25)";
-//         search_btn.className = search_btn.className.replace(new RegExp(/^icon-btn/), "nested-icon-btn");
-//     }
-//     else {
-//         search_box.style.display = "inline-block";
-//         // search_box.classList.toggle("hide", true);
-//     }
-// }
+let search_box = document.getElementById("search-input");
+let search_btn = document.getElementById("search-btn");
+let search_container = document.getElementById("search-container");
+let search_form = document.querySelector("#search-container form");
+
+search_form.addEventListener("mouseenter", () => {
+    console.log("enter");
+    search_form.classList.toggle("searchform-displayed", true);
+});
+
+search_form.addEventListener("mouseleave", hide_searchbar);
+search_box.addEventListener("focusout", hide_searchbar);
+
+function hide_searchbar() {
+    if (search_box !== document.activeElement && search_box.value === "") {
+        search_form.classList.toggle("searchform-displayed", false);
+
+    }
+}
