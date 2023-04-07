406deed7bd14ea82a09a584cd8964a230d915aba
Alexdev8 <alexisbensoussan06@gmail.com>
2023-04-07 20:00:14 +0100

    animation de la searchbar et espacement des boutons

---
+++ scripts/index_script.js
@@ -0,0 +1,20 @@
+// let search_box = document.getElementById("search-input");
+// let search_btn = document.getElementById("search-btn");
+// let search_div = document.getElementById("search-div");
+//
+// let search_box_opened = false;
+// function openSearchBox() {
+//     if (!search_box_opened) {
+//         search_box_opened = true;
+//         // search_box.style.display = "inline-block";
+//         search_box.classList.toggle("show");
+//         console.log(search_box.classList);
+//         search_div.style.padding = "0.3em";
+//         search_div.style.boxShadow = "2px 2px 6px rgb(26,26,25)";
+//         search_btn.className = search_btn.className.replace(new RegExp(/^icon-btn/), "nested-icon-btn");
+//     }
+//     else {
+//         search_box.style.display = "inline-block";
+//         // search_box.classList.toggle("hide", true);
+//     }
+// }
