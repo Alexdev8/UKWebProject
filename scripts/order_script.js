3ac88063a802fe3a110396c4d09ab7a7e094a138
Alexdev8 <alexisbensoussan06@gmail.com>
2023-04-10 19:59:38 +0100

    datepicker dans order.html

--- scripts/order_script.js
+++ scripts/order_script.js
@@ -1,20 +1,32 @@
-// let search_box = document.getElementById("search-input");
-// let search_btn = document.getElementById("search-btn");
-// let search_container = document.getElementById("search-container");
-// let search_form = document.querySelector("#search-container form");
-//
-// search_form.addEventListener("mouseenter", () => {
-//     console.log("enter");
-//     search_form.classList.toggle("searchform-displayed", true);
-// });
-//
-// search_form.addEventListener("mouseleave", hide_searchbar);
-// search_box.addEventListener("focusout", hide_searchbar);
-//
-// function hide_searchbar() {
-//     if (search_box !== document.activeElement && search_box.value === "") {
-//         search_form.classList.toggle("searchform-displayed", false);
-//
-//     }
-// }
+let ticket_form = document.forms["ticket-form"];
+let date_input_div = document.getElementsByClassName("date-input-div")[0];
+
+date_input_div.addEventListener("click", evt => {
+    console.log(evt.target);
+    if (evt.target !== ticket_form["ticket-start-date"] && ticket_form["ticket-start-date"].value === "") {
+        open_picker();
+    }
+})
+
+function open_picker() {
+    ticket_form["ticket-start-date"].showPicker();
+    ticket_form["ticket-start-date"].focus();
+}
+
+function enable_form_input() {
+    if (ticket_form["ticket-start-date"].value !== "") {
+        ticket_form["ticket-end-date"].disabled = false;
+        ticket_form["ticket-end-date"].focus();
+        ticket_form["ticket-end-date"].showPicker();
+    }
+    else {
+        disable_form_input();
+    }
+}
+
+function disable_form_input() {
+    ticket_form["ticket-end-date"].value = "";
+    ticket_form["ticket-end-date"].disabled = true;
+}
+
 console.log("order_script");
