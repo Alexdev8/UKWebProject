d66e894d52e94fc6e10b29e11a0cd0018600da42
Alexdev8 <alexisbensoussan06@gmail.com>
2023-04-12 19:05:42 +0100

    selection du type de ticket dans order.html

--- scripts/order_script.js
+++ scripts/order_script.js
@@ -1,32 +1,58 @@
-let ticket_form = document.forms["ticket-form"];
+let order_form = document.forms["order-form"];
+let ticket_type_selection = document.getElementsByClassName("ticket-type-selection")[0];
+let ticket_booking = document.getElementById("ticket-booking");
+let hotel_reservation = document.getElementById("hotel-reservation");
+let restaurant_reservation = document.getElementById("restaurant-reservation");
+
 let date_input_div = document.getElementsByClassName("date-input-div")[0];
 let start_date_input_div = document.getElementById("start-date-input-div");
 let end_date_input_div = document.getElementById("end-date-input-div");
 
 const today = new Date();
 const max_reservation_date = addYears(new Date(), 2);
+let ticket_type;
 
-ticket_form["ticket-start-date"].min = today.toLocaleDateString('fr-ca');
-ticket_form["ticket-start-date"].max = max_reservation_date.toLocaleDateString('fr-ca');
+function select_ticket_type(type) {
+    document.querySelector("body").style.overflow = 'hidden';
+    ticket_type = type;
+    ticket_type_selection.style.height = '0';
+    ticket_type_selection.style.opacity = '0';
+    ticket_type_selection.style.transform = "translateY(-200%)";
+    ticket_booking.style.display = "block";
+    setTimeout(function () {
+        ticket_type_selection.style.display = "none";
+        ticket_booking.classList.toggle("hidden", false);
+    }, 400);
+    switch (type) {
+        case "dated-ticket":
+            break;
+        case "special-ticket":
+            break;
+        default:
+            break;
+    }
+}
+
+init_start_date_input();
 
 date_input_div.addEventListener("click", evt => {
-    if (evt.target !== ticket_form["ticket-start-date"] && ticket_form["ticket-start-date"].value === "") {
+    if (evt.target !== order_form["reservation-start-date"] && order_form["reservation-start-date"].value === "") {
         open_picker();
         return;
     }
-    if (evt.target === start_date_input_div && !ticket_form["ticket-start-date"].hasFocus) {
-        ticket_form["ticket-start-date"].focus();
+    if (evt.target === start_date_input_div && !order_form["reservation-start-date"].hasFocus) {
+        order_form["reservation-start-date"].focus();
         return;
     }
-    if (evt.target === end_date_input_div && !ticket_form["ticket-end-date"].hasFocus) {
-        ticket_form["ticket-end-date"].focus();
+    if (evt.target === end_date_input_div && !order_form["reservation-end-date"].hasFocus) {
+        order_form["reservation-end-date"].focus();
         return;
     }
 })
 
 function open_picker() {
-    ticket_form["ticket-start-date"].showPicker();
-    ticket_form["ticket-start-date"].focus();
+    order_form["reservation-start-date"].showPicker();
+    order_form["reservation-start-date"].focus();
 }
 
 function addYears(date, years) {
@@ -34,17 +60,22 @@ function addYears(date, years) {
     return date;
 }
 
+function init_start_date_input() {
+    order_form["reservation-start-date"].min = today.toLocaleDateString('fr-ca');
+    order_form["reservation-start-date"].max = max_reservation_date.toLocaleDateString('fr-ca');
+}
+
 function init_end_date_input() {
-    ticket_form["ticket-end-date"].min = ticket_form["ticket-start-date"].value;
-    ticket_form["ticket-end-date"].max = max_reservation_date.toLocaleDateString('fr-ca');
+    order_form["reservation-end-date"].min = order_form["reservation-start-date"].value;
+    order_form["reservation-end-date"].max = max_reservation_date.toLocaleDateString('fr-ca');
 }
 
 function enable_form_input() {
-    if (ticket_form["ticket-start-date"].value !== "") {
+    if (order_form["reservation-start-date"].value !== "") {
         init_end_date_input();
-        ticket_form["ticket-end-date"].disabled = false;
-        ticket_form["ticket-end-date"].focus();
-        ticket_form["ticket-end-date"].showPicker();
+        order_form["reservation-end-date"].disabled = false;
+        order_form["reservation-end-date"].focus();
+        order_form["reservation-end-date"].showPicker();
     }
     else {
         disable_form_input();
@@ -52,8 +83,6 @@ function enable_form_input() {
 }
 
 function disable_form_input() {
-    ticket_form["ticket-end-date"].value = "";
-    ticket_form["ticket-end-date"].disabled = true;
+    order_form["reservation-end-date"].value = "";
+    order_form["reservation-end-date"].disabled = true;
 }
-
-console.log("order_script");
