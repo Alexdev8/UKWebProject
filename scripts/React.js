8a39ce0272fda26e42c4878ab02b2121cc145bac
D00Dth <tom.beaupuis@efrei.net>
2023-04-21 01:59:55 +0200

    footer et une partie du carousel en react

--- scripts/React.js
+++ scripts/React.js
@@ -1,6 +1,8 @@
 const React = require('react');
+import React from "react";
 
-const Logo = function (props){
+/* HEADER */
+const Logo = function (){
     const handleClick = () => {
         window.location.href = "index.html";
     }
@@ -14,7 +16,7 @@ const Logo = function (props){
     </div>
 };
 
-const SearchBar = function (props){
+const SearchBar = function (){
     return <div id="search-container">
             <form action="">
                 <label><input type="search" id="search-input" role="searchbox" name="search_input" placeholder={"Search"}/></label>
@@ -23,11 +25,11 @@ const SearchBar = function (props){
     </div>
 }
 
-const DarkMode = function (props){
+const DarkMode = function (){
     return <span id="dark_mode-btn" className="icon-btn material-symbols-outlined"> dark_mode </span>
 }
 
-const Settings = function (props){
+const Settings = function (){
     const handleClick = () => {
         window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }
@@ -35,11 +37,11 @@ const Settings = function (props){
 
 }
 
-const Account = function (props){
+const Account = function (){
     return <span id="account-btn" className="icon-btn material-symbols-outlined"> person </span>
 }
 
-const HeaderSide = function (props){
+const HeaderSide = function (){
     return(
         <div className="header-side" id="right-side">
             <SearchBar />
@@ -51,7 +53,7 @@ const HeaderSide = function (props){
     )
 }
 
-const Header = function (props){
+const Header = function (){
     return (
         <div id="header">
             <div className="header-side"></div>
@@ -59,5 +61,99 @@ const Header = function (props){
             <HeaderSide />
         </div>
     )
+};
+
+/* CAROUSEL */
+
+const TitleCarousel = function (){
+    return(
+        <a href="attractions.html"><h1>Our great attractions</h1></a>
+    )
+};
+
+function CarouselIndicators({ buttonCount }){
+    const buttons = [];
+
+    for (let i = 0; i < buttonCount; i++){
+        const isActive = i === 0 ? 'active' : '';
+        buttons.push(
+            <button
+                key={index}
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to={index}
+                aria-label={`Slide ${index + 1}`}
+            ></button>
+        );
+    }
+    return(
+        <div className="carousel-indicators">
+            {buttons}
+        </div>
+    );
 }
 
+<ButtonCarousel buttonCount={5} />
+/* It will create 5 buttons */
+
+function PictureCarousel({ numberOfPictures,URLAttraction, URLPicture, name, Description}){
+    const pictures = [];
+
+    for (let i = 0; i < numberOfPictures; i++){
+        pictures.push(
+            <div className="carousel-item">
+                <a href={URLPicture}>
+                    <img src={URLAttraction} className="carousel-img"/>
+                    <div className="carousel-caption">
+                        <h5>{name}</h5>
+                        <p>{Description}</p>
+                    </div>
+                </a>
+            </div>
+        );
+    }
+    return(
+        <div className="carousel-inner">
+            {pictures}
+        </div>
+        )
+}
+
+<PictureCarousel
+    numberOfPictures={3},
+    URLAttraction={"attractions.html"},
+    URLPicture={"https://focus.courrierinternational.com/2022/02/04/0/0/5568/3712/1280/0/60/0/a82f9b4_1644008435467-000-9ba2ne.jpg"},
+    name={"Attraction 1"},
+    Description={"A nice one"}
+/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* FOOTER */
+
+const Footer = function (props){
+    return (
+        <div> <hr/>
+            <a href="contact-us.html">Contact us</a>
+            <a href="privacy-policy.html">Privacy policy</a>
+            <a href="about.html">About</a>
+            <a href="careers/jobs.html">Job offers</a>
+            <div>
+                © 2023 RoseCrown • All rights reserved.
+            </div>
+        </div>
+    )
+}
