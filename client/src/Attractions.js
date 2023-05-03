58f7e59a00aaaef4595ee110177bbf991e558432
Tekitsu <ouguergouzidriss@gmail.com>
2023-05-03 15:58:43 +0200

    Page attraction un petit à jour

--- client/src/Attractions.js
+++ client/src/Attractions.js
@@ -5,28 +5,37 @@ const listItems = [
     {
         id: "One",
         ariaExpanded: true,
-        name: "disney",
+        name: "100 years war",
         description: "parc d'attraction lie a l'univers disney",
-        picture: "",
-        link: "https://www.disneylandparis.com/fr-fr/attractions/",
-        alt: "disney-logo"
+        picture: "https://images.lindependant.fr/api/v1/images/view/5d54055fd286c202264fd612/large/image.jpg?v=1",
+        link: "",
+        alt: "Reconstitution photo"
     },
     {
         id: "Two",
         ariaExpanded: false,
-        name: "Parc Asterix",
-        description: "parc de gaulois",
-        picture: "",
+        name: "Vikings Invasion",
+        description: "",
+        picture: "https://leisureopportunities.co.uk/images/971601_409024.jpg",
         link: "https://www.parcasterix.fr/?gclid=CjwKCAjwuqiiBhBtEiwATgvixC_Juz7DtddvhwgH0ald1r_148R6fH401kQs7AuxY28ldVlxfBZ7thoCM8IQAvD_BwE&gclsrc=aw.ds",
         alt: "asterix-logo"
     },
     {
         id: "Three",
         ariaExpanded: false,
-        name: "Puy du fou",
-        description: "parc historique",
+        name: "Construction of Hadrian's Wall",
+        description: "",
         picture: "",
-        link: "https://www.puydufou.com/france/fr",
+        link: "https://static.nationalgeographic.fr/files/styles/image_3200/public/gettyimages-486453968.jpg?w=1900&h=1267",
+        alt: "puy-du-fou-logo"
+    },
+    {
+        id: "Four",
+        ariaExpanded: false,
+        name: "Construction of Hadrian's Wall",
+        description: "",
+        picture: "",
+        link: "https://static.nationalgeographic.fr/files/styles/image_3200/public/gettyimages-486453968.jpg?w=1900&h=1267",
         alt: "puy-du-fou-logo"
     }
 ]
