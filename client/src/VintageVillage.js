302348e2c7ee54e9021a52eff80aeb803516b492
D00Dth <tom.beaupuis@efrei.net>
2023-05-07 17:45:35 +0200

    modification vintage village

--- client/src/VintageVillage.js
+++ client/src/VintageVillage.js
@@ -1,25 +1,27 @@
+import {Card2} from "./Shared_components"
 const VintageVillageIMG = [
     {
         id: "romanVillage",
-        img: "https://congres.puydufou.com/sites/default/files/styles/max_width_1200/public/2020-09/carrousel_3_recadre_5.jpg?itok=kesu_QIK",
+        src: "https://congres.puydufou.com/sites/default/files/styles/max_width_1200/public/2020-09/carrousel_3_recadre_5.jpg?itok=kesu_QIK",
         title: "The Roman Village",
-        description: "In the Roman village, you'll see the impressive architecture and engineering feats that defined this civilization. Marvel at the grandeur of the aqueduct, imagine yourself as a gladiator in the arena, and explore the marketplace where merchants sell exotic goods from all corners of the empire."
+        description: "ezrgar"
+        //In the Roman village, you'll see the impressive architecture and engineering feats that defined this civilization. Marvel at the grandeur of the aqueduct, imagine yourself as a gladiator in the arena, and explore the marketplace where merchants sell exotic goods from all corners of the empire.
     },
     {
         id: "vikingVillage",
-        img: "https://www.la-poze-travel.com/wp-content/uploads/2019/09/fort-de-lan-mil-puy-du-fou-3-min.jpg",
+        src: "https://www.la-poze-travel.com/wp-content/uploads/2019/09/fort-de-lan-mil-puy-du-fou-3-min.jpg",
         title: "The Viking Village",
         description: ""
     },
     {
         id: "medievalVillage",
-        img: "https://www.atlantic-loire-valley.com/sites/default/files/styles/slide_article_classique/public/mediatheque/images/Puy-du-Fou-La-Cite-Medievale.jpg?h=84071268&itok=o9PAlazb",
+        src: "https://www.atlantic-loire-valley.com/sites/default/files/styles/slide_article_classique/public/mediatheque/images/Puy-du-Fou-La-Cite-Medievale.jpg?h=84071268&itok=o9PAlazb",
         title: "The Medieval Village",
         description: ""
     },
     {
         id: "XIXCenturiesVillage",
-        img: "https://prod-sites-pdf-webdata.s3.eu-west-1.amazonaws.com/france/files/s3fs-public/styles/show_carrousel_image_desktop/public/shows/2016-09/bourg01.JPG?VersionId=eSJBX8G0TIWQomO6bDSPrcyFjaFRuJUe&itok=69ZZuu9L",
+        src: "https://prod-sites-pdf-webdata.s3.eu-west-1.amazonaws.com/france/files/s3fs-public/styles/show_carrousel_image_desktop/public/shows/2016-09/bourg01.JPG?VersionId=eSJBX8G0TIWQomO6bDSPrcyFjaFRuJUe&itok=69ZZuu9L",
         title: "The XIX centuries Village",
         description: ""
     }
@@ -38,18 +40,7 @@ function VintageVillageImg({img}){
                             <button>Send</button>
                     </form>
                 </div>
-                {img.map((img) => (
-                    <div id={img.id}>
-                        <img src={img.img} alt="..."/>
-                        <div className="image-overlay">
-                            <p>
-                                {img.title}
-                                <br/>
-                                {img.description}
-                            </p>
-                        </div>
-                    </div>
-                ))}
+                <Card2 cards={VintageVillageIMG} />
             </div>
         </section>
         )
