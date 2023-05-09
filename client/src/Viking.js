d4d55b053ce3416b8334ffcf4a7ba63cac0dc099
clemoll <60341116+clemoll@users.noreply.github.com>
2023-05-09 13:02:43 +0200

    Màj attraction

--- client/src/Viking.js
+++ client/src/Viking.js
@@ -3,18 +3,18 @@ import {Link, useNavigate} from "react-router-dom";
 
 const Viking = () => {
     return (
-            <div className="viking-content content-section">
+            <div className="viking-content">
                 <section id="viking-introduction">
                     <div className="viking-container">
                         <h1>Bienvenue à l'Attraction Viking !</h1>
                         <p>Découvrez une aventure passionnante dans l'univers des Vikings. Embarquez dans notre navire à la découverte de paysages époustouflants et de créatures légendaires.</p>
-                        <button onClick={() => navigate("/order/tickets")}>Get a ticket now</button>
+                        <button className={"button-viking"} onClick={() => navigate("/order/tickets")}>GET A TICKET NOW</button>
                     </div>
                 </section>
 
                 <section id="viking-perso">
                     <div className="perso-viking">
-                        <img src={require("./resources/persoviking.PNG")} alt="persoviking"></img>
+                        <img src={require("./resources/persoviking.png")} alt="persoviking"></img>
                     </div>
                 </section>
 
