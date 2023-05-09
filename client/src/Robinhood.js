7823c455c64f7470d3c9b93d3ae6f0e51a283b7d
clemoll <60341116+clemoll@users.noreply.github.com>
2023-05-09 14:52:47 +0200

    Màj attraction

--- client/src/Robinhood.js
+++ client/src/Robinhood.js
@@ -1,34 +1,36 @@
 import React from "react";
+import {Link, useNavigate} from "react-router-dom";
 
 const Robinhood = () => {
     return (
-
         <div className="robinhood-content">
             <section id="robinhood-introduction">
                 <div className="robinhood-container">
-                    <h1>Bienvenue à l'Attraction Robin hood !</h1>
-                    <p>Découvrez une aventure passionnante dans l'univers de robin des bois. parcourez la foret de sherwood à la découverte de paysages époustouflants et de créations étonnantes.</p>
-                    <button>Réserver maintenant</button>
+                    <h1>Bienvenue à l'Attraction Viking !</h1>
+                    <p>Découvrez une aventure passionnante dans l'univers des Vikings. Embarquez dans notre navire à la découverte de paysages époustouflants et de créatures légendaires.</p>
+                    <button className={"button-robinhood"} onClick={() => navigate("/order/tickets")}>Get A Ticket Now</button>
                 </div>
             </section>
 
             <section id="robinhood-perso">
-                <div className="robinhood-perso">
-                    <img src="./resources/persoviking.PNG" alt="persorobinhood"></img>
+                <div className="perso-robinhood">
+                    <img src={require("./resources/robin_hood.png")} alt="persorobinhood"></img>
                 </div>
             </section>
 
             <section id="robinhood-description">
                 <div className="robinhood-container">
                     <h2>Description</h2>
-                    <p>Lorem ipsum dolor sit amet. Sit cumque molestiae et earum illum sed deserunt porro et voluptas aliquid ut nemo doloremque. Ex voluptatem illum sit earum ipsum ut nihil enim hic ipsum reprehenderit. Et tenetur ullam eum dolorem corrupti aut nisi aperiam. Nam quas debitis aut voluptatem aperiam sit dicta neque sit quod distinctio ut dolore iusto.</p>
+                    <p>L'Attraction Viking est une expérience immersive dans la culture nordique. Le voyage commence dans le fjord de Bergen, où les visiteurs embarquent sur un navire viking authentique. Pendant le trajet, les passagers pourront profiter de la vue sur les montagnes et les fjords, ainsi que sur la faune et la flore locale.</p>
+                    <p>Après quelques minutes de navigation, le navire arrive sur une ile mystérieuse où les visiteurs peuvent explorer un village viking reconstitué. Ils pourront découvrir comment les Vikings vivaient, travaillaient et se battaient. Les visiteurs pourront également rencontrer des artisans qui recréent des objets du quotidien, des bijoux et des armes.</p>
+                    <p>Après avoir visité le village, les visiteurs retourneront sur le navire pour un retour au fjord de Bergen. Pendant le trajet de retour, les passagers pourront profiter de spectacles de combat de gladiateurs vikings, de chants traditionnels et de danses.</p>
                 </div>
             </section>
 
             <section id="robinhood-histoire">
                 <div className="robinhood-container">
                     <h2>Histoire</h2>
-                    <p>Lorem ipsum dolor sit amet. Sit cumque aliquid ut nemo doloremque. Ex voluptatem illum sit earum ipsum ut nihil enim hic ipsum reprehenderit. Et tenetur ullam eum dolorem corrupti aut nisi aperiam. Nam quas debitis aut voluptatem aperiam sit dicta neque sit quod distinctio ut dolore iusto.</p>
+                    <p>L'Attraction Viking a été créée pour permettre aux visiteurs de découvrir la culture nordique de manière immersive et interactive. Le parc a travaillé avec des historiens pour recréer les navires, les bâtiments et les objets du quotidien avec</p>
                 </div>
             </section>
         </div>
--- client/src/Roman.js
+++ client/src/Roman.js
@@ -8,13 +8,13 @@ const Roman = () => {
                 <div className="roman-container">
                     <h1>Bienvenue à l'Attraction Romaine !</h1>
                     <p>Découvrez une aventure passionnante dans l'univers des Romains. Longez le mur d'Hadrien à la découverte de paysages époustouflants et de créations étonnantes.</p>
-                    <button>Réserver maintenant</button>
+                    <button className={"button-roman"} onClick={() => navigate("/order/tickets")}>GET A TICKET NOW</button>
                 </div>
             </section>
 
             <section id="roman-perso">
-                <div className="roman-viking">
-                    <img src={require("./resources/persoviking.PNG")} alt="persoroman"></img>
+                <div className="perso-roman">
+                    <img src={require("./resources/perso-roman.png")} alt="persoroman"></img>
                 </div>
             </section>
 
